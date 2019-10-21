19002794b69e010e9a7c535b6d3d45ac439fc6de
kaiguang.wang <nx-engine2018>
2019-10-21 14:29:34 +0800

    服务提交

---
+++ eureka-service/src/main/java/springclouddemo/demo/EurekaServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package springclouddemo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceDemoApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-ribbon/src/main/java/springcloudribbon/demo/RibbonApplication.java
@@ -0,0 +1,26 @@
+package springcloudribbon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableEurekaClient
+@EnableDiscoveryClient
+@SpringBootApplication
+public class RibbonApplication {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonApplication.class, args);
+    }
+
+}
